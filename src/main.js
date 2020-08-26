6b0da8797b3a6e0d4bfd623c47b2bb5f78acd3a6
杰哥哥 <85723773@qq.com>
2020-08-26 18:21:33 +0800

    配置了项目中的vant

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './style/base.less'
+import './style/iconfont.css'
+import { Button } from 'vant'
+
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
